6e6dc6646a5198c10fc010d9a2fc70799372fea5
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-10-02 20:15:52 +0700

    init project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import { HeroUIProvider } from "@heroui/react";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <HeroUIProvider>
+        <Component {...pageProps} />
+      </HeroUIProvider>
+    </SessionProvider>
+  );
+}
